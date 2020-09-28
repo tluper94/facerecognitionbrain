54f24099c5c2639368c5deb93d2cd2c74339311a
Trevor Luper <luper4@gmail.com>
2020-09-28 11:09:19 -0500

    Intial Commit

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './icons8-brain-100.png';
+import './logo.css';
+
+const Logo = () => {
+  return (
+    <div className='ma4 mt0'>
+      <Tilt
+        className='Tilt br4 shadow-2 '
+        options={{ max: 35 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className='Tilt-inner pa3'>
+          <img style={{ paddingTop: '5px' }} alt='brain' src={brain}></img>
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+export default Logo;
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <nav className='flex justify-end'>
+      <p className='f2 link dim black underline pa3 pointer'>Sign Out</p>
+    </nav>
+  );
+};
+export default Navigation;
