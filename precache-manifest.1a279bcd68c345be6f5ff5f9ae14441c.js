f483c1feac9f551f052f62b3f8307a014468b92b
Trevor Luper <luper4@gmail.com>
2020-10-01 00:36:09 -0500

    Updates

---
+++ precache-manifest.1a279bcd68c345be6f5ff5f9ae14441c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7466c3d19bda584bf0ddee342c19cfd",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "65d66b387dd2136b56e6",
+    "url": "/facerecognitionbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e94a5de2b04a0014dc79",
+    "url": "/facerecognitionbrain/static/css/main.86eda624.chunk.css"
+  },
+  {
+    "revision": "65d66b387dd2136b56e6",
+    "url": "/facerecognitionbrain/static/js/2.6f8fbe7d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionbrain/static/js/2.6f8fbe7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94a5de2b04a0014dc79",
+    "url": "/facerecognitionbrain/static/js/main.1d4937a7.chunk.js"
+  },
+  {
+    "revision": "a0d072c450e7e6f545a0",
+    "url": "/facerecognitionbrain/static/js/runtime-main.3b236050.js"
+  }
+]);
